550d32d0dd5d1030a61bf2dd9aaf9242adbfe4fd
shachisuryavanshi <shachisingh.ay@gmail.com>
2026-06-05 03:17:16 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/blooddonor/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/blooddonor/backend/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(List.of("x-auth-token"));
