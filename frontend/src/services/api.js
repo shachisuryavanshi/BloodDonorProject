e62d6b0e888345e26fa279b9ce184ac4a3e5d198
shachisuryavanshi <shachisingh.ay@gmail.com>
2026-06-05 03:06:34 +0530

    Update api.js
    added url of backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://blooddonorproject-production.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
